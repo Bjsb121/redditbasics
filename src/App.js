89b5cda39393f89cdfba21a97a4c7bcbd117b91b
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-04-10 17:32:34 +0200

    Everything working so far

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Reddit Basics
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <input type="text" placeholder="Search Reddit" />
+        <h1>Reddit Basics</h1>
       </header>
     </div>
   );
